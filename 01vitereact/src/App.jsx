5fb4ff6679309a052c692e37b223d4174bd245b2
Anand123098sitare <ap9012928@gmail.com>
2025-12-21 23:13:36 +0530

    created using vite

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+
+import Chai from './chai.jsx';
+
+function App() {
+  return (
+    <Chai/>
+  )
+}
+
+export default App
+
+// we can only return one element from a component
+// to return multiple elements we can use a div or a Fragment
+// Fragment syntax: <> </>
